69358f326ec80841c2b730f61f76053a94f56a13
fadzlil <azmifadz111@gmail.com>
2019-08-19 10:59:12 +0800

    Handle supportSDK for android

--- android/src/main/java/com/codeheadlabs/zendesk/ZendeskPlugin.java
+++ android/src/main/java/com/codeheadlabs/zendesk/ZendeskPlugin.java
@@ -6,11 +6,17 @@ import com.zopim.android.sdk.api.ZopimChat;
 import com.zopim.android.sdk.model.VisitorInfo;
 import com.zopim.android.sdk.prechat.ZopimChatActivity;
 
+import io.flutter.Log;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
+import zendesk.core.AnonymousIdentity;
+import zendesk.core.Identity;
+import zendesk.core.Zendesk;
+import zendesk.support.Support;
+import zendesk.support.guide.HelpCenterActivity;
 
 /** ZendeskPlugin */
 public class ZendeskPlugin implements MethodCallHandler {
@@ -32,11 +38,17 @@ public class ZendeskPlugin implements MethodCallHandler {
       case "init":
         handleInit(call, result);
         break;
+      case "initSupportSDK":
+        handleInitSupportSDK(call, result);
+        break;
       case "setVisitorInfo":
         handleSetVisitorInfo(call, result);
         break;
       case "startChat":
         handleStartChat(call, result);
+        break;
+      case "startSupportSDK":
+        handleStartSupportSDK(call, result);
         break;
       default:
         result.notImplemented();
@@ -46,6 +58,35 @@ public class ZendeskPlugin implements MethodCallHandler {
 
   private void handleInit(MethodCall call, Result result) {
     ZopimChat.init((String) call.argument("accountKey"));
+    result.success(true);
+  }
+
+  private void handleInitSupportSDK(MethodCall call, Result result) {
+
+    String appId = "";
+    String clientId = "";
+    String url = "";
+
+    if (call.hasArgument("appId")) {
+      appId = (String) call.argument("appId");
+    }
+    if (call.hasArgument("clientId")) {
+      clientId = (String) call.argument("clientId");
+    }
+    if (call.hasArgument("url")) {
+      url = (String) call.argument("url");
+    }
+
+    Log.d("appId", appId);
+    Log.d("clientId", clientId);
+    Log.d("url", url);
+
+    Zendesk.INSTANCE.init(mRegistrar.activeContext(), url, appId, clientId);
+    Identity identity = new AnonymousIdentity();
+    Zendesk.INSTANCE.setIdentity(identity);
+
+    Support.INSTANCE.init(Zendesk.INSTANCE);
+
     result.success(true);
   }
 
@@ -70,6 +111,13 @@ public class ZendeskPlugin implements MethodCallHandler {
   private void handleStartChat(MethodCall call, Result result) {
     Intent intent = new Intent(mRegistrar.activeContext(), ZopimChatActivity.class);
     mRegistrar.activeContext().startActivity(intent);
+    result.success(true);
+  }
+
+  private void handleStartSupportSDK(MethodCall call, Result result) {
+
+    HelpCenterActivity.builder()
+            .show(mRegistrar.activeContext());
     result.success(true);
   }
 }
